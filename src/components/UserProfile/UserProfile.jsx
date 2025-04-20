b898df44d3ba9a3d2006dace1b02ada13a78917c
Beverly Green <greenbev000@gmail.com>
2025-04-20 18:15:27 -0500

    small comma typo in json fixture file fixed, username is accessible in profile page to display username

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,16 +1,20 @@
 import { useLocation } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import mockResultData from '../../data/QuestionnaireSubmissionData.json'
 
 function UserProfile() {
     const location = useLocation()
     const navigate = useNavigate()
     const goHome = () => navigate("/welcome")
     const { currentUser } = location.state || {}
-    // console.log(currentUser) //current user is null, keeping since I will edit this code on another branch
-    // const username = currentUser.attributes.username
+    const [recommendedPets, setRecommendedPets] = useState([])
+    console.log(currentUser) 
+    // //current user is null, keeping since I will edit this code on another branch
+    const username = currentUser.attributes.username
     return (
         <main>
-            <h1> Pet MatchMaker Profile 🐾</h1>
+            <h1> {username}'s' Pet MatchMaker Profile 🐾</h1>
           <nav>
               <button onClick= {goHome}> Welcome Page</button>
           </nav>
