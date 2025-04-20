001063c02f214fdc4b39959dbfc14e1346bda501
Beverly Green <greenbev000@gmail.com>
2025-04-20 15:24:10 -0500

    Welcome page is visible with mission statement and nav bar buttons to go to questionnaire and profile

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,23 @@
+import { useNavigate } from "react-router";
+
+
 function WelcomePage() {
+    const navigate = useNavigate()
+    const goToProfile = () => navigate("/profile")
+    const goToQuestionnaire = () => navigate("/questionnaire")
     return (
       <main>
-        <h1>Welcome Page</h1>
-        <p>You made it! 🎉</p>
-      </main>
+      <header>
+          <h1>Pet MatchMaker 🐾</h1>
+          <nav>
+              <button onClick={goToQuestionnaire}>Questionnaire</button>
+              <button onClick={goToProfile}>Profile</button>
+          </nav>
+      </header>
+      <div>
+          "Choosing a furry (or maybe scaly!) companion is an exciting decision for any future pet owner. This app helps simplify that process by guiding users through a short lifestyle questionnaire, then recommending the pet that best fits their daily routine, living space, budget, and personal preferences. By promoting thoughtful pet selection, we hope to strengthen the bond between people and their future companions, leading to happier lives for both."
+      </div>
+    </main>
     );
   }
   
