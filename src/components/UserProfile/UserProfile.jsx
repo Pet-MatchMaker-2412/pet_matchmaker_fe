84d582f29c3cd3af2726b83aed257a7fa0dee80d
Beverly Green <greenbev000@gmail.com>
2025-04-20 16:39:27 -0500

    connected welcome page to profile page

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,12 +1,19 @@
 import { useLocation } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function UserProfile() {
     const location = useLocation()
-    console.log("Location state:", location.state);
+    const navigate = useNavigate()
+    const goHome = () => navigate("/welcome")
     const { currentUser } = location.state || {}
+    // console.log(currentUser) //current user is null, keeping since I will edit this code on another branch
+    // const username = currentUser.attributes.username
     return (
         <main>
-            <h1>Your Profile</h1>
+            <h1> Pet MatchMaker Profile 🐾</h1>
+          <nav>
+              <button onClick= {goHome}> Welcome Page</button>
+          </nav>
         </main>
     );
 }
