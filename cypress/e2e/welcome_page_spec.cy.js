a184b4227c3ef241eef03cb49e2d77efdcaae580
Beverly Green <greenbev000@gmail.com>
2025-04-22 15:45:58 -0500

    Added several tests for welcome page, refactored onClicks to Links

--- cypress/e2e/welcome_page_spec.cy.js
+++ cypress/e2e/welcome_page_spec.cy.js
@@ -2,8 +2,21 @@ describe('Welcome Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/welcome')
   })
-  it('displays title on page load', () => {
+
+  it('displays title', () => {
     cy.get('h1')
     cy.contains('Pet MatchMaker 🐾')
   })
+
+  it('displays the mission statement', () => {
+    cy.get('div')
+    cy.contains('Choosing a furry (or maybe scaly!) companion is an exciting decision for any future pet owner. This app helps simplify that process by guiding users through a short lifestyle questionnaire, then recommending the pet that best fits their daily routine, living space, budget, and personal preferences. By promoting thoughtful pet selection, we hope to strengthen the bond between people and their future companions, leading to happier lives for both.')
+  })
+
+  it('displays a footer with credits', () => {
+    cy.get('footer')
+    cy.contains('Put any necessary credits here in the footer.')
+  })
+
+  it('has ')
 })
--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function WelcomePage() {
     const navigate = useNavigate()
@@ -10,14 +11,20 @@ function WelcomePage() {
       <header>
           <h1>Pet MatchMaker 🐾</h1>
           <nav>
-              <button onClick= {goToResources}> Resources</button>
-              <button onClick={goToProfile}>Profile</button>
+          <Link to="/resources">
+            <button>Resources</button>
+          </Link>
+          <Link to="/profile">
+            <button>Profile</button>
+          </Link>
           </nav>
       </header>
       <div>
           Choosing a furry (or maybe scaly!) companion is an exciting decision for any future pet owner. This app helps simplify that process by guiding users through a short lifestyle questionnaire, then recommending the pet that best fits their daily routine, living space, budget, and personal preferences. By promoting thoughtful pet selection, we hope to strengthen the bond between people and their future companions, leading to happier lives for both.
       </div>
-      <button onClick={goToQuestionnaire}>Take the Quiz!</button>
+      <Link to="/questionnaire">
+        <button>Take the Quiz!</button>
+      </Link>
       <footer>
         Put any necessary credits here in the footer. 
       </footer>
