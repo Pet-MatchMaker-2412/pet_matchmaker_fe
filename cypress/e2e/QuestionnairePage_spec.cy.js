0fbc45d753e7c2277ae06b1285eed0ee97a83308
Logan Sauer <ldsauer2@gmail.com>
2025-04-22 20:44:39 -0600

    finished up the testing

--- cypress/e2e/QuestionnairePage_spec.cy.js
+++ cypress/e2e/QuestionnairePage_spec.cy.js
@@ -4,13 +4,41 @@ describe('QuestionnairePage spec', () => {
   })
 
   it("should display the nav buttons and question content", () => {
-    cy.get('h1').should("contain", "Pet MatchMaker")
-    .get("button").contains("Home").should("exist")
-    .get("button").contains("Profile").should("exist")
+    cy.get("h1")
+    .should("contain", "Pet MatchMaker")
+
+    cy.get("button")
+    .contains("Home")
+    .should("exist")
+    
+    cy.get("button")
+    .contains("Profile")
+    .should("exist")
   })
   
   it("checks that the QuestionCard is being rendered", () => {
-    cy.get("form").should("exist")
-    .get("button[type='submit']").should("exist")
+    cy.get("form")
+    .should("exist")
+    
+    cy.get("button[type='submit']")
+    .should("exist")
+  })
+
+  it("should navigate to the home page when clicking the home button", () => {
+    cy.get("button")
+    .contains("Home")
+    .click()
+    
+    cy.url()
+    .should("include", "/welcome")
+  })
+
+  it("should navigate to the profile page when clicking the profile button", () => {
+    cy.get("button")
+    .contains("Profile")
+    .click()
+
+    cy.url()
+    .should("include", "/profile")
   })
 })
