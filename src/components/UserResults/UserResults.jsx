8c53fbc6fbd26d2baa33edffcf0d617d71d73945
Will Fox <william.macdonald.fox@gmail.com>
2025-04-24 19:42:26 -0400

    feat: import useEffect in UserResults

--- src/components/UserResults/UserResults.jsx
+++ src/components/UserResults/UserResults.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function UserResults({ currentUser matchResults, saveMatch }) {
