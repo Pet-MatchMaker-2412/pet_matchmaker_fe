1b11afa9f4ce0a811cd81b48a0c0d881b923c858
Beverly Green <greenbev000@gmail.com>
2025-04-25 18:13:36 -0500

    made homepage paragraph larger

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -14,15 +14,12 @@ function WelcomePage() {
                 </Link>
           </nav>
       </header>
-      <div>
+      <h3>
           Choosing a furry (or maybe scaly!) companion is an exciting decision for any future pet owner. This app helps simplify that process by guiding users through a short lifestyle questionnaire, then recommending the pet that best fits their daily routine, living space, budget, and personal preferences. By promoting thoughtful pet selection, we hope to strengthen the bond between people and their future companions, leading to happier lives for both.
-      </div>
+      </h3>
       <Link to="/questionnaire">
             <button>Take the Quiz!</button>
         </Link>
-      <footer>
-        Put any necessary credits here in the footer. 
-      </footer>
     </main>
     )
   }
