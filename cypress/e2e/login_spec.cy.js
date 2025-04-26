da8d52fc72c9d4d734c13f99b8cda945dc182cd4
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-26 13:37:20 -0700

    feat: updating login cypress

--- cypress/e2e/login_spec.cy.js
+++ cypress/e2e/login_spec.cy.js
@@ -1,22 +1,19 @@
-// logans old PR code that Bev copied
+describe('LoginPage spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/')
+  })
 
+  it("should display the login form elements", () => {
+    cy.get('h1').should("contain", "Welcome")
+    cy.get('h2').should("contain", "Login or Sign Up")
+    cy.get('input[placeholder="Enter Username"]').should("exist")
+    cy.get('button').contains("Login").should("exist")
+    cy.get('button').contains("Sign Up").should("exist")
+  })
 
-// describe('LoginPage spec', () => {
-//   beforeEach(() => {
-//      cy.visit('http://localhost:5173/')
-//    })
- 
-//    it("should display the login form with input and login button", () => {
-//      cy.get('h1').should("contain", "Login")
-//      .get('input[placeholder="Enter Username"]')
-//      .get('button').contains("Login").should("exist")
-//      .get('h1').should("contain", "Login")
-//    })
- 
-//    it("should allow a suer to enter a username and login", () => {
-//      cy.get("input[placeholder='Enter Username']").type('testuser')
-//      .get("button").contains("Login").click()
-//      cy.url().should("include", "/welcome")
-//    })
- 
-//  })
+  it("should allow a user to enter a username and login", () => {
+    cy.get('input[placeholder="Enter Username"]').type('testuser')
+    cy.get('button').contains('Login').click()
+    cy.url().should('include', '/welcome')
+  })
+})
